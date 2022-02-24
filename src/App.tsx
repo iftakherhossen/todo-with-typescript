83b7c5b793298a2af1263ede2495b7daaaada2a6
iftakherone <54303402+iftakherone@users.noreply.github.com>
2022-02-24 23:37:10 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Todo } from './pages/model';
+import TodoList from './pages/TodoList/TodoList';
+import { FaCheck } from 'react-icons/fa';
+import { AiFillDelete } from 'react-icons/ai';
 
-function App() {
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todoLists, setTodoLists] = useState<Todo[]>([]);
+  const addHandle = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodoLists([...todoLists, { id: Date.now(), todo, isDone: false }]);
+      localStorage.setItem('todoList', JSON.stringify([...todoLists, { id: Date.now(), todo, isDone: false }]))
+      setTodo('')
+    }
+  }
+  useEffect(() => {
+    const getItem: any = localStorage.getItem('todoList');
+    if (getItem) {
+      setTodoLists(JSON.parse(getItem))
+    }
+  }, [])
+  const handleDone = (id: number) => {
+    setTodoLists(
+      todoLists.map(todoList => todoList.id === id ? { ...todoList, isDone: !todoList.isDone } : todoList)
+    )
+  }
+  const handleDelete = (id: number) => {
+    setTodoLists(
+      todoLists.filter(todoList => todoList.id !== id)
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <TodoList todo={todo} setTodo={setTodo} addHandle={addHandle}></TodoList>
+
+      <div className='container' >
+        <div className="row d-flex justify-content-center align-items-center flex-column mt-4">
+          {
+            todoLists.map((todoList, index) =>
+              <div className="col-lg-4 col-md-7 col-12" key={index}>
+                <div className="alert alert-light" role="alert">
+                  <div className='d-flex justify-content-between align-items-center'>
+                    {
+                      todoList.isDone ? <s className='text-black fs-4'>{todoList.todo}</s> : <span className='text-black fs-4'>{todoList.todo}</span>
+                    }
+                    <div className='d-flex align-items-center'>
+                      <span style={{ cursor: 'pointer' }} className='me-lg-3 me-md-3 me-2 text-success'
+                        onClick={() => handleDone(todoList.id)}
+                      ><FaCheck /></span>
+                      <span
+                        onClick={() => handleDelete(todoList.id)}
+                        style={{ cursor: 'pointer' }} className='text-danger'><AiFillDelete /></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+        </div>
+      </div>)
+
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/TodoList/TodoList.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    addHandle: (e: React.FormEvent) => void;
+}
+
+const TodoList: React.FC<Props> = ({ todo, setTodo, addHandle }) => {
+
+    return (
+        <div className='container'>
+            <h1 className='text-center text-white heading pt-3'>Todo List</h1>
+            <form onSubmit={addHandle}>
+                <div className="row d-flex justify-content-center align-items-center">
+                    <div className="col-lg-4 col-md-8 col-10 position-relative d-flex justify-content-center align-items-center ">
+                        <input className='form-control border border-3 rounded-pill' type="input" placeholder='write your daily task' value={todo} onChange={(e) => setTodo(e.target.value)} />
+                        <input className='btn btn-primary position-absolute end-0 border border-3 rounded-pill' type="submit" value='Add' />
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default TodoList;
